ab2297af66b58e5c58560ee9b6ec6af6edbb594e
pablotroncoso <pablotroncoso@gmail.com>
2022-06-07 18:46:32 -0400

    NavBar de nuevo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,18 @@
-<><img src="./img/opticalogo.jpg" width="150px" alt="Gran Óptica Gran!" /><div id="app">
+{/* <><img src="./img/opticalogo.jpg" width="150px" alt="Gran Óptica Gran!" /><div id="app">
     <h2>
         <a href="modelos.html">Modelos</a>
         <a href="cristales.html">Cristales</a>
         <a href="filtros.html">Filtros</a>
         <a href="contacto.html">Contacto</a>
-    </h2></</>
+    </h2></</> */}
+
+    function NavBar () {
+        return (
+            <nav>
+                <ul>
+                    <li> Categoría A</li>
+                    <li> Categoría A</li>
+                </ul>
+            </nav>
+        );
+    }
