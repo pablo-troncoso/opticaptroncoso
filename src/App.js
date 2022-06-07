47775c9191ed3838085559ebfc6e62f51a27b382
pablotroncoso <pablotroncoso@gmail.com>
2022-06-07 19:56:25 -0400

    Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import LogoImg from './img/opticalogo.jpg';
+import NavBar from './components/NavBar/NavBar';
 function App() {
+
+
+  const stylesH1 = {
+    color: 'blue',
+  }
+
   return (
     <div className="App">
-    Primer After Class  
-
+      <header className="App-header">
+        <NavBar/>
+        <h1 style={stylesH1}>
+        BIenvenidos a mi APP
+        </h1>
+        <img alt="logo optica" src={LogoImg}/>
+        <br/>
+        <hr/>
+        <p style={{marginTop:"120px"}}>Hola Hola</p>
+      </header>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+      <nav className="nav-main">
+          <ul>
+              <li> Categoría A</li>
+              <li> Categoría A</li>
+            </ul>
+        </nav>
+    );
+}
