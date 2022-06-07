aadb2384120af78669b62b023ad692c2729522f6
pablotroncoso <pablotroncoso@gmail.com>
2022-06-07 00:51:22 -0400

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,7 @@
+<><img src="./img/opticalogo.jpg" width="150px" alt="Gran Óptica Gran!" /><div id="app">
+    <h2>
+        <a href="modelos.html">Modelos</a>
+        <a href="cristales.html">Cristales</a>
+        <a href="filtros.html">Filtros</a>
+        <a href="contacto.html">Contacto</a>
+    </h2></</>
